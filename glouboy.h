1d218a4966d91d5a9767ac5275c3296fb330e382
Joseph pinkasfeld <joseph.pinkasfeld@gmail.com>
2020-02-20 00:27:17 +0100

    first commit

---
+++ glouboy.h
@@ -0,0 +1,21 @@
+//
+//  glouboy.h
+//  imguiex-osx
+//
+//  Created by pinkasfeld on 30/11/2017.
+//  Copyright © 2017 Joel Davis. All rights reserved.
+//
+
+#ifndef glouboy_h
+#define glouboy_h
+
+#include <stdio.h>
+
+namespace Glouboy {
+	void init();
+	void update();
+	void execute();
+}
+
+
+#endif /* glouboy_h */
